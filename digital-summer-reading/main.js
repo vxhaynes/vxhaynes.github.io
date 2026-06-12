2f854e989f3093e7b26be0a4de97bfa1e88fae97
vxhaynes <108424335+vxhaynes@users.noreply.github.com>
2026-06-12 13:23:08 -0400

    Delete digital-summer-reading directory

--- digital-summer-reading/main.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".cover-wrap img").forEach((image) => {
-  const markMissing = () => image.closest(".cover-wrap")?.classList.add("cover-missing");
-
-  if (image.complete && image.naturalWidth === 0) {
-    markMissing();
-  }
-
-  image.addEventListener("error", markMissing);
-});
